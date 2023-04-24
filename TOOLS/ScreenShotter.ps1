2b503baf11acc5737ceb6e1a6f7c95c791d795cd
Steven <111704953+I-DoNt-LiKe-Micro-Soft@users.noreply.github.com>
2023-04-24 23:18:16 +0100

    Add files via upload

---
+++ TOOLS/ScreenShotter.ps1
@@ -0,0 +1,22 @@
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+$screens = [Windows.Forms.Screen]::AllScreens
+$top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
+$left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
+$width  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
+$height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
+$bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
+$bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
+$graphics = [Drawing.Graphics]::FromImage($bmp)
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+$Path = "$pwd\"
+while($True){
+    $Count = 0
+    while($True){
+        $Count++
+        $bmp.Save("${Path}${Count}.png")
+        if($Count -eq 1000){break;}
+    }
+    Remove-Item *.png
+}
+$graphics.Dispose()
+$bmp.Dispose()
